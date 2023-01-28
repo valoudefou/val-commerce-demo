5fc5fb0d60293b870c1de21391b54995414a15c0
valoudefou <valerian.karsenty@gmail.com>
2023-01-28 11:42:48 +0000

    Change headline

--- components/Header.js
+++ components/Header.js
@@ -30,7 +30,7 @@ export default function Header({ scrollHandler }) {
                 className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-orange-600 shadow-sm hover:bg-orange-100 sm:px-8"
                 onClick={scrollHandler}
               >
-                Shop coffees
+                Shop catalog
               </button>
             </div>
           </div>
