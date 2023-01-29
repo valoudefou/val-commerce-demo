ed57bca8afe8e45cda735057fa2f10e90bf6e014
valoudefou <valerian.karsenty@gmail.com>
2023-01-29 14:22:15 +0000

    GTM deployed

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,17 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
+        {/* <Script strategy="afterInteractive" src="https://try.abtasty.com/1ceff369b6cd9aceaa9ee318e6498167.js" /> */}
+        <Script strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TR4C6NR');`}}></Script>
           <meta
             name="description"
             content="The premiere coffee delivery service."
@@ -15,6 +22,8 @@ class MyDocument extends Document {
           />
         </Head>
         <body>
+        <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TR4C6NR"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
           <Main />
           <NextScript />
         </body>
