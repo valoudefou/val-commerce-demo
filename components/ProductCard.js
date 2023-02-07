93cc93169b77ddf4c079a14709c3d1cf2f0d9224
valoudefou <valerian.karsenty@gmail.com>
2023-02-07 20:48:27 +0000

    JSON

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,7 +1,20 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import useSWR from 'swr'
+
+//Write a fetcher function to wrap the native fetch function and return the result of a call to url in json format
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function ProductCard() {
+      //Set up SWR to run the fetcher function when calling "/api/staticdata"
+  //There are 3 possible states: (1) loading when data is null (2) ready when the data is returned (3) error when there was an error fetching the data
+  const { data, error } = useSWR('/api/staticdata', fetcher);
+    //Handle the error state
+    if (error) return <div>Failed to load</div>;
+    //Handle the loading state
+    if (!data) return <div>Loading...</div>;
+    //Handle the ready state and display the result contained in the data object mapped to the structure of the json file
+console.log(JSON.parse(data))
 return (
 <Link href="/products/pretty-woman-ring-mini-model" className="group">
 <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
