fc0d8e95d8df2b5ef80de01d7b5b293ae12af18f
valoudefou <valerian.karsenty@gmail.com>
2023-02-15 09:07:45 +0000

    Styling

--- components/Footer.js
+++ components/Footer.js
@@ -6,12 +6,12 @@ const flagBackgroundColor = useFsFlag("flagBackgroundColor", "black")
 return (
 <>
 <main>{children}</main>
-<footer className="center items-center mt-5 flex justify-center space-x-4 bg-[#E7E8EF] p-4 text-xs fixed inset-x-0 bottom-0 h-25 z-50 font-normal">
+<footer style={{background: flagBackgroundColor.getValue()}} className="center items-center mt-5 flex justify-center space-x-4 bg-[#E7E8EF] p-4 text-xs fixed inset-x-0 bottom-0 h-25 z-50 font-normal">
 <p>Powered by Next.js / Vercel / Tailwind CSS / Epoq / Flagship by AB Tasty</p>
 <span>|</span>
 <a
 href="https://github.com/valoudefou/commerce-demo"
-style={{color: flagBackgroundColor.getValue()}}
+className="text-gray-600 text-[13px]"
 >
 Source code
 </a>
