dbf9cc37037ce8742bfd253282eb701bd104cec6
valoudefou <valerian.karsenty@gmail.com>
2023-02-18 23:53:45 +0000

    FONT

--- components/Navbar.js
+++ components/Navbar.js
@@ -24,7 +24,7 @@ className="text-2xl px-7 font-bold leading-relaxed inline-block mr-4 py-3 whites
 href="/"
 >
 {flagIndustry.getValue()}
-<span className="text-xs font-thin py-1 absolute">®</span>
+<span className="text-sm font-thin py-1 absolute">®</span>
 {/* <span className="text-2xl font-normal py-1 absolute ml-10 bg-red-600 p-10 text-white">
 {my_test_flag.getValue()}
 {my_test_flag.getValue()}
@@ -50,7 +50,7 @@ id="example-navbar-danger"
 <li className="nav-item">
 
 <a
-className="my-1 text-xs text-gray-900 hover:text-gray-700 font-light md:mx-4 md:my-0"
+className="my-1 text-sm text-gray-900 hover:text-gray-700 font-light md:mx-4 md:my-0"
 href="/"
 >
 <span className="ml-2">Home</span>
@@ -58,7 +58,7 @@ href="/"
 </li>
 <li className="nav-item">
 <a
-className="my-1 text-xs text-gray-900 hover:text-gray-700 font-light md:mx-4 md:my-0"
+className="my-1 text-sm text-gray-900 hover:text-gray-700 font-light md:mx-4 md:my-0"
 href="/"
 >
 <span className="ml-2">Shop</span>
@@ -66,7 +66,7 @@ href="/"
 </li>
 <li className="nav-item">
 <a
-className="my-1 text-xs text-gray-900 hover:text-gray-700 font-light md:mx-4 md:my-0"
+className="my-1 text-sm text-gray-900 hover:text-gray-700 font-light md:mx-4 md:my-0"
 href="/"
 >
 <span className="ml-2">Contact</span>
@@ -74,7 +74,7 @@ href="/"
 </li>
 <li className="nav-item">
 <a
-className="my-1 text-xs text-gray-900 hover:text-gray-700 font-light md:mx-4 md:my-0"
+className="my-1 text-sm text-gray-900 hover:text-gray-700 font-light md:mx-4 md:my-0"
 href="/"
 >
 <span className="ml-2">About</span>
@@ -88,7 +88,7 @@ className="flex justify-center px-7 py-3 md:block">
 <svg className="h-6 w-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M3 3H5L5.4 5M7 13H17L21 5H5.4M7 13L5.4 5M7 13L4.70711 15.2929C4.07714 15.9229 4.52331 17 5.41421 17H17M17 17C15.8954 17 15 17.8954 15 19C15 20.1046 15.8954 21 17 21C18.1046 21 19 20.1046 19 19C19 17.8954 18.1046 17 17 17ZM9 19C9 20.1046 8.10457 21 7 21C5.89543 21 5 20.1046 5 19C5 17.8954 5.89543 17 7 17C8.10457 17 9 17.8954 9 19Z" stroke="currentColor" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round"></path>
 </svg>
-<span style={{ backgroundColor: flagBackgroundColor.getValue() }} className="absolute top-0 left-0 rounded-full bg-indigo-100 text-gray-900 p-1 text-xs"></span>
+<span style={{ backgroundColor: flagBackgroundColor.getValue() }} className="absolute top-0 left-0 rounded-full bg-indigo-100 text-gray-900 p-1 text-sm"></span>
 </a>
 </div>
 </div>
