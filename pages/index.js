af29288bdce37e2d25bb06592aca7b96b0b8245f
valoudefou <valerian.karsenty@gmail.com>
2023-06-11 22:18:40 +0100

    FEED

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,8 @@ export default function Index( {products, articles, sheetdata} ) {
 console.log(products)
 
 
-    const t = sheetdata.slice(1).map(([productId,name,brand,price,oldprice,category,productUrl,smallImage,availability,quantity,size,google_product_category]) => ({ productId,name,brand,price,oldprice,category,productUrl,smallImage,availability,quantity,size,google_product_category }) )
-    console.log(t)
+    // const t = sheetdata.slice(1).map(([productId,name,brand,price,oldprice,category,productUrl,smallImage,availability,quantity,size,google_product_category]) => ({ productId,name,brand,price,oldprice,category,productUrl,smallImage,availability,quantity,size,google_product_category }) )
+    // console.log(t)
 
 let coffeeRef = useRef()
 const scrollHandler = (e) => {
@@ -58,8 +58,8 @@ export async function getStaticProps() {
         })
 const res = await fetch('https://dummyjson.com/products')
 const data = await res.json()
-const req1 = await fetch('https://nextjs-abtasty.vercel.app/api/sheet');
-const res1 = await req1.json();
+// const req1 = await fetch('https://nextjs-abtasty.vercel.app/api/sheet');
+// const res1 = await req1.json();
 const art = await client.getEntries({
     content_type: 'articles'
     })
@@ -67,7 +67,7 @@ return {
 props: {
 products: data,
 articles: art.items,
-sheetdata: res1.data
+// sheetdata: res1.data
 },
 }
 }
