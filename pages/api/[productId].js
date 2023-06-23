84906fadf5e3c9e3802036ce658cb7ad3a3303b5
valoudefou <valerian.karsenty@gmail.com>
2023-06-23 13:46:35 +0100

    API

--- pages/api/[productId].js
+++ pages/api/[productId].js
@@ -2,6 +2,6 @@ import { products } from sheetdata
 
 export default function handler(req, res) {
     const { productId } = req.query
-    const product = products.find( product => product.id === parseInt(productId))
+    const product = products.find( product => product.productId === parseInt(productId))
     res.status(200).json(product)
 }
