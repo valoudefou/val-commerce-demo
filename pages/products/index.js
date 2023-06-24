8a555c4e7d4fe306385eeddb43b96d514db5e6e1
valoudefou <valerian.karsenty@gmail.com>
2023-06-24 10:17:25 +0100

    PRODUCTS

--- pages/products/index.js
+++ pages/products/index.js
@@ -18,8 +18,8 @@ Shop our products
 </div>
 <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
 <>
-{products.products.map((product) => (
-<ProductCard product={product} key={product.id} /> 
+{products.map((product) => (
+<ProductCard product={product} key={product.productId} /> 
 ))}
 </>
 </div>
@@ -29,7 +29,7 @@ Shop our products
 }
 
 export async function getStaticProps() {
-const res = await fetch('https://dummyjson.com/products')
+const res = await fetch('https://nextjs-abtasty.vercel.app/api/products')
 const data = await res.json()
 console.log(data)
 return {
