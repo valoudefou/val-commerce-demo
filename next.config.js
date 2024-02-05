12a834196b1240fb2772a39a47fa20cbb057c645
valoudefou <111360917+valoudefou@users.noreply.github.com>
2024-02-05 10:57:09 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['i.dummyjson.com', 'cdn.shopify.com', 'api.ecom.longines.com'],
+    domains: ['cdn.dummyjson.com', 'cdn.shopify.com', 'api.ecom.longines.com'],
   },
 }
