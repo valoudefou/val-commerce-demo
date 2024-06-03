cea3d981abd55fe9daf2962b7d0f06dd8a56302d
valoudefou <valerian.karsenty@gmail.com>
2024-06-03 10:45:28 +0200

    ATC TEXT

--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -54,7 +54,7 @@ className="flex items-center justify-center text-sm font-base mt-5 border border
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 py-0.5">
 <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
 </svg>
-Checkout
+Add To Cart
 </button>
 </Link>
 <>
