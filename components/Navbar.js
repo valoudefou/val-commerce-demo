42edeec67f04f5ff60f9197afb224a3feba3ba53
valoudefou <valerian.karsenty@gmail.com>
2024-06-12 18:32:57 +0100

    CART STATE

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,9 @@ import SlidingCart from "./SlidingCart"
 import MiniCart from "./MiniCart"
 import Link from 'next/link'
 
+
 export default function Navbar() {
+
 
 
 const [isShown, setIsShown] = useState(false)
@@ -100,7 +102,9 @@ className="flex justify-center px-7 py-3 md:block">
 <svg className="h-6 w-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M3 3H5L5.4 5M7 13H17L21 5H5.4M7 13L5.4 5M7 13L4.70711 15.2929C4.07714 15.9229 4.52331 17 5.41421 17H17M17 17C15.8954 17 15 17.8954 15 19C15 20.1046 15.8954 21 17 21C18.1046 21 19 20.1046 19 19C19 17.8954 18.1046 17 17 17ZM9 19C9 20.1046 8.10457 21 7 21C5.89543 21 5 20.1046 5 19C5 17.8954 5.89543 17 7 17C8.10457 17 9 17.8954 9 19Z" stroke="currentColor" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round"></path>
 </svg>
+
 <span style={{ backgroundColor: flagBackgroundColor.getValue() }} className="absolute top-0 left-0 rounded-full bg-indigo-100 text-gray-900 p-1 text-sm"></span>
+
 </a>
 </div>
 </div>
