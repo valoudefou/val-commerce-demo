921d5e812ec19f8011ac185de86a5904bd2bd83c
valoudefou <valerian.karsenty@gmail.com>
2024-06-18 15:16:32 +0100

    CLEAN CODE

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -4,54 +4,54 @@ import { useState } from 'react'
 import { HitType, useFlagship, useFsFlag } from "@flagship.io/react-sdk"
 
 function ProductCard( {product} ) {
-    const fs = useFlagship()
     const viewDetailsPlp = useFsFlag("viewDetailsPlp", false)
     const [isLoading, setLoading] = useState(true)
     const { hit: fsHit } = useFlagship()
- 
+
     function cn(...classes) {
         return classes.filter(Boolean).join(' ')
     }
 
     return (
-    <Link href={`/products/${product.id}`} className="group">
-            
-        <div className='flex flex-col'>
-            <div className="flex-1 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-            <Image
-            alt=""
-            src={product.images[0]}
-            layout='fill'
-            objectFit="cover"
-            className={cn(
-                'duration-700 ease-in-out group-hover:opacity-75',
-                isLoading
-                ? 'scale-110 blur-2xl grayscale'
-                : 'scale-100 blur-0 grayscale-0'
-            )}
-            onLoadingComplete={() => setLoading(false)}
-            />
-        </div>
+        <Link href={`/products/${product.id}`} className="group">
+            <div className='flex flex-col'>
+                <div className="flex-1 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+                    <Image
+                        alt=""
+                        src={product.images[0]}
+                        layout='fill'
+                        objectFit="cover"
+                        className={cn(
+                            'duration-700 ease-in-out group-hover:opacity-75',
+                            isLoading
+                            ? 'scale-110 blur-2xl grayscale'
+                            : 'scale-100 blur-0 grayscale-0'
+                        )}
+                        onLoadingComplete={() => setLoading(false)}
+                    />
+                </div>
 
-        <div className="mt-4 flex items-center justify-between text-base font-normal text-gray-900">
-            <h3>{product.title}</h3>
-            <p className='font-base font-bold'>{product.price}€</p>
-        </div>
+                <div className="mt-4 flex items-center justify-between text-base font-normal text-gray-900">
+                    <h3>{product.title}</h3>
+                    <p className='font-base font-bold'>{product.price}€</p>
+                </div>
 
-        {viewDetailsPlp.getValue() === true &&
-            <button onClick={()=>{
-                fsHit.send({
-                type: HitType.EVENT,
-                category: "Action Tracking",
-                action: "Click View Details PLP",
-                label: "Engagement"
-                })
-                }} className="flex items-center justify-center text-sm font-base mt-5 border border-black bg-white px-4 py-4 text-black sm:px-8 w-full">
-                View details
-            </button>
-        }
-        </div>
-    </Link>
+                {viewDetailsPlp.getValue() === true &&
+                    <button onClick={()=>{
+
+                        fsHit.send({
+                            type: HitType.EVENT,
+                            category: "Action Tracking",
+                            action: "Click View Details PLP",
+                            label: "Engagement"
+                        })
+
+                    }} className="flex items-center justify-center text-sm font-base mt-5 border border-black bg-white px-4 py-4 text-black sm:px-8 w-full">
+                    View details
+                    </button>
+                }
+            </div>
+        </Link>
     )
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -9,108 +9,99 @@ import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps, initialFlagsData, initialVisitorData }) {
 
-useEffect(() => {
+    useEffect(() => {
 
-    if (typeof window !== 'undefined') {
-        const antiFlicker = document.querySelector('#ab-tasty-anti-flicker')
+        if (typeof window !== 'undefined') {
+            const antiFlicker = document.querySelector('#ab-tasty-anti-flicker')
 
-        if (antiFlicker && window.ABTasty !== 'undefined') {
-            window.addEventListener('abtasty_executedCampaign', (event) => {
-                // console.log(event.detail)
-                function antiFlicker() {
-                    const antiFlicker = document.querySelector('#ab-tasty-anti-flicker')  
-                    antiFlicker.style.visibility = 'hidden'
-                }
-                setTimeout(antiFlicker, 500)
-            })
+            if (antiFlicker && window.ABTasty !== 'undefined') {
+
+                window.addEventListener('abtasty_executedCampaign', (event) => {
+
+                    function antiFlicker() {
+                        const antiFlicker = document.querySelector('#ab-tasty-anti-flicker')  
+                        antiFlicker.style.visibility = 'hidden'
+                    }
+                    setTimeout(antiFlicker, 500)
+                })
+            } 
         } 
-    } 
 
-    // anti flicker when AB Tasty is not defined
-    window.addEventListener('load', () => {
-        const antiFlicker = document.querySelector('#ab-tasty-anti-flicker') 
+        // Anti-flicker when AB Tasty is not defined
+        window.addEventListener('load', () => {
+            const antiFlicker = document.querySelector('#ab-tasty-anti-flicker') 
 
-        if (antiFlicker) {
-            antiFlicker.style.visibility = 'hidden'
-        }
-    })
-    
-}, [])
+            if (antiFlicker) {
+                antiFlicker.style.visibility = 'hidden'
+            }
 
-//get flag 
-const fs = useFlagship()
-const flagIndustry = useFsFlag("flagIndustry", "Product")
-return (
-<>
-{/* <div>
-      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-      <button onClick={() => setName("")}>Clear</button>
-    </div> */}
-<FlagshipProvider
-onVisitorExposed={({ exposedVisitor, fromFlag }) => {
-    window.abtasty.send("segment", {
-        s: {
-          userId: exposedVisitor.id,
-          flagInfo: fromFlag.metadata
-        }
-    })
-}}
-envId={process.env.NEXT_PUBLIC_FS_ENV}
-apiKey={process.env.NEXT_PUBLIC_FS_KEY}
-visitorData={initialVisitorData}
-initialFlagsData={initialFlagsData || {}}>
+        })
+        
+    }, [])
 
-<Head />
-<title>{'The ' + flagIndustry.getValue() + ' House'}</title>
-<Component {...pageProps} />
-<Footer />
-</FlagshipProvider>
-</>
-)
+    // Get flag 
+    const fs = useFlagship()
+    const flagIndustry = useFsFlag("flagIndustry", "Product")
+
+    return (
+        <>
+        <FlagshipProvider
+        envId={process.env.NEXT_PUBLIC_FS_ENV}
+        apiKey={process.env.NEXT_PUBLIC_FS_KEY}
+        visitorData={initialVisitorData}
+        initialFlagsData={initialFlagsData || {}}>
+        <Head />
+        <title>{'The ' + flagIndustry.getValue() + ' House'}</title>
+        <Component {...pageProps} />
+        <Footer />
+        </FlagshipProvider>
+        </>
+    )
 }
+
 MyApp.getInitialProps = async (appContext) => {
-const appProps = await App.getInitialProps(appContext)
+    const appProps = await App.getInitialProps(appContext)
 
-//Start the Flagship SDK
-const flagship = Flagship.start(process.env.NEXT_PUBLIC_FS_ENV, process.env.NEXT_PUBLIC_FS_KEY, {
-    fetchNow: false,
-})
+    // Start the Flagship SDK
+    const flagship = Flagship.start(process.env.NEXT_PUBLIC_FS_ENV, process.env.NEXT_PUBLIC_FS_KEY, {
+        fetchNow: false,
+    })
 
-const initialVisitorData = {
-// id: 'test20',
-id: uuidv4(),
-context: {
-    organisation: "whatever",
-    device: 'mobile',
-    store: 'US',
-    subscription: 'true',
-    page: '/product',
-    segment: 'coffee',
-    member: 'true',
-    beta: 'test',
-    product: '20948209482098490284',
-    login: 'true',
-    book: 'test',
-    sku: '92842942398',
-    ios: "true",
-    regionId: 3,
-    android: '0.0.3',
-    },
-}
-// Create a new visitor
-const visitor = flagship?.newVisitor({
-    visitorId: initialVisitorData.id,
-    context: initialVisitorData.context,
-})
+    const initialVisitorData = {
+        // id: 'test20',
+        id: uuidv4(),
+            context: {
+            organisation: "whatever",
+            device: 'mobile',
+            store: 'US',
+            subscription: 'true',
+            page: '/product',
+            segment: 'coffee',
+            member: 'true',
+            beta: 'test',
+            product: '20948209482098490284',
+            login: 'true',
+            book: 'test',
+            sku: '92842942398',
+            ios: "true",
+            regionId: 3,
+            android: '0.0.3',
+        },
+    }
+    // Create a new visitor
+    const visitor = flagship?.newVisitor({
+        visitorId: initialVisitorData.id,
+        context: initialVisitorData.context,
+    })
 
-//Fetch flags
-await visitor?.fetchFlags()
+    // Fetch flags
+    await visitor?.fetchFlags()
 
-// Pass data to the page via props
-return {
-    ...appProps,
-    initialFlagsData: visitor?.getFlagsDataArray(),
-    initialVisitorData,
+    // Pass data to the page via props
+    return {
+        ...appProps,
+        initialFlagsData: visitor?.getFlagsDataArray(),
+        initialVisitorData,
     }
 }
 
