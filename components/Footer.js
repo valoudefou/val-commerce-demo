a1a9441ab0fb2020bd56f1224692c58ed7059651
valoudefou <valerian.karsenty@gmail.com>
2024-06-19 11:43:08 +0100

    FOOTER CLEAN CODE

--- components/Footer.js
+++ components/Footer.js
@@ -1,47 +1,22 @@
-// import { HitType, useFlagship, useFsFlag } from "@flagship.io/react-sdk"
-
-// export default function Footer({ children }) {
-// const fs = useFlagship();
-// const flagBackgroundColor = useFsFlag("flagBackgroundColor", "black")
-// return (
-// <>
-// <main>{children}</main>
-// <footer className="center items-center mt-5 flex justify-center space-x-4 bg-[#E7E8EF] p-4 text-xs fixed inset-x-0 bottom-0 h-25 z-50 font-normal">
-// <p>Powered by Next.js / Vercel / Tailwind / Contentful / AB Tasty / EmotionsAI</p>
-// <span>|</span>
-// <a
-// href="https://github.com/valoudefou/val-commerce-demo"
-// style={{color: flagBackgroundColor.getValue()}}
-// >
-// GitHub
-// </a>
-// </footer>
-// </>
-// )
-// }
-
-
-import { HitType, useFlagship, useFsFlag } from "@flagship.io/react-sdk"
-import { useEffect, useState } from "react";
+import { useFlagship, useFsFlag } from "@flagship.io/react-sdk"
 
 export default function Footer({ children }) {
-const fs = useFlagship();
-const flagBackgroundColor = useFsFlag("flagBackgroundColor", "black")
-return (
-<>
-<main>{children}</main>
-<footer className="center items-center mt-5 flex justify-center space-x-4 bg-[#E7E8EF] p-4 text-xs fixed inset-x-0 bottom-0 h-25 z-50 font-normal">
-<p>Powered by Next.js / Vercel / Tailwind / Contentful / AB Tasty / EmotionsAI</p>
-<span>|</span>
-<a
-href="https://github.com/valoudefou/commerce-demo"
-style={{color: flagBackgroundColor.getValue()}}
-className="p-[2px] bg-[#111827] rounded-[5px]"
->
-GitHub
-</a>
-</footer>
-</>
-)
+    const fs = useFlagship()
+    const flagBackgroundColor = useFsFlag("flagBackgroundColor", "black")
+
+    return (
+        <>
+            <main>
+                {children}
+            </main>
+            <footer className="center items-center mt-5 flex justify-center space-x-4 bg-[#E7E8EF] p-4 text-xs fixed inset-x-0 bottom-0 h-25 z-50 font-normal">
+                <p>Powered by Next.js / Vercel / Tailwind / AB Tasty</p>
+                    <span>|</span>
+                <a href="https://github.com/valoudefou/commerce-demo" style={{color: flagBackgroundColor.getValue()}} className="p-[2px] bg-[#111827] rounded-[5px]">
+                    GitHub
+                </a>
+            </footer>
+        </>
+    )
 }
 
