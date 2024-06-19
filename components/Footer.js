3754dfd7b25888e4c26f9ba03b67f262ebdc2a52
valoudefou <valerian.karsenty@gmail.com>
2024-06-19 12:17:47 +0100

    FOOTER BUTTON

--- components/Footer.js
+++ components/Footer.js
@@ -12,7 +12,7 @@ export default function Footer({ children }) {
             <footer className="center items-center mt-5 flex justify-center space-x-4 bg-[#E7E8EF] p-4 text-xs fixed inset-x-0 bottom-0 h-25 z-50 font-normal">
                 <p>Next.js / Vercel / Tailwind / AB Tasty</p>
                     <span>|</span>
-                <a href="https://github.com/valoudefou/commerce-demo" style={{color: flagBackgroundColor.getValue()}} className="p-[2px] bg-[#111827] rounded-[5px]">
+                <a href="https://github.com/valoudefou/commerce-demo" style={{color: flagBackgroundColor.getValue()}} className="p-1 font-semibold text-xs bg-[#111827] rounded-[5px]">
                     GitHub
                 </a>
             </footer>
