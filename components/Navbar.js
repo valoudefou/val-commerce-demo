1b49d58e04381c4d2d658ec9f8362b5106d6d29a
valoudefou <valerian.karsenty@gmail.com>
2024-06-20 22:59:33 +0100

    FIX RESPONSIVE

--- components/Navbar.js
+++ components/Navbar.js
@@ -39,26 +39,16 @@ export default function Navbar() {
                             className="text-gray-900 cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none" type="button">
                         </button>
                     </div>
-                    <div className="items-center px-4 justify-center hidden sm:flex">
-                        <div className="flex relative">
+                    <div className="items-center px-4 justify-center sm:flex">
+                        <div className="hidden md:flex relative">
                             <div className="absolute top-3 left-3 items-center">
                                 <svg className="w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
                             </div>
-                            <input type="text" className="epoq_search_box w-96 ui-autocomplete-input block p-2 font-light pl-10 w-70 text-gray-900 bg-gray-50 rounded border border-gray-200 focus:pl-10 lowercase" placeholder={'Search ' + flagIndustry.getValue() + ' ...'} />
+                            <input type="text" className="epoq_search_box w-96 ui-autocomplete-input block p-2 font-light pl-10 text-gray-900 bg-gray-50 rounded border border-gray-200 focus:pl-10" placeholder={'Search ' + flagIndustry.getValue() + ' ...'} />
                         </div>
                     </div>
-                    <div className="lg:flex items-center">
+                    <div className=" lg:flex items-center">
                         <ul className="hidden flex-col lg:flex lg:flex-row list-none">
-                            <li className="nav-item">
-                                <a className="my-1 text-sm text-gray-900 hover:text-gray-700 font-light md:mx-4 md:my-0" href="/">
-                                <span className="ml-2">{isShown}</span>
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="my-1 text-sm text-gray-900 hover:text-gray-700 font-light md:mx-4 md:my-0" href="/">
-                                <span className="ml-2">Shop</span>
-                                </a>
-                            </li>
                             <li className="nav-item">
                                 <a className="my-1 text-sm text-gray-900 hover:text-gray-700 font-light md:mx-4 md:my-0" href="/">
                                     <Link href='/blog/content-1'><span className="ml-2">Blog</span></Link>
