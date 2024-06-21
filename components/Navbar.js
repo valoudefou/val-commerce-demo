f413bd163e6f08d76b26c376990ad1f75ef002f4
valoudefou <valerian.karsenty@gmail.com>
2024-06-22 00:58:53 +0100

    AND OPERATOR

--- components/Navbar.js
+++ components/Navbar.js
@@ -116,7 +116,7 @@ export default function Navbar() {
                     </div>
                 </div>
             </nav>
-            {!burgerOn && ( 
+            {!burgerOn && !isShown && ( 
                 <Footer />
             )}
             {isShown && flagCartFeature.getValue() === 'MiniCart' && (    
