151af160b4f0ef60afbed16af577ae7a3b555066
valoudefou <valerian.karsenty@gmail.com>
2024-06-21 16:51:13 +0100

    RESPONSIVE

--- components/Navbar.js
+++ components/Navbar.js
@@ -14,7 +14,7 @@ export default function Navbar() {
         if (storedHtml) {
             setHtmlContent(true)
         } 
-        
+
         else  if (!storedHtml) {
             setHtmlContent(false)
   
@@ -31,16 +31,13 @@ export default function Navbar() {
             <nav className="relative z-40 flex flex-wrap items-center justify-between px-2 py-2 bg-white border-b-[1px] border-gray-200">
                 <div className="flex flex-auto lg:flex-wrap items-center justify-between">
                     <div className="relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-                        <a className="text-2xl px-7 font-bold leading-relaxed inline-block mr-4 py-3 whitespace-nowrap uppercase text-gray-900" href="/">
+                        <a className="text-2xl px-7 font-bold leading-relaxed inline-block py-3 whitespace-nowrap uppercase text-gray-900" href="/">
                             {flagIndustry.getValue()}
                             <span className="text-sm font-thin py-1 absolute">®</span>
                         </a>
-                        <button
-                            className="text-gray-900 cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none" type="button">
-                        </button>
                     </div>
                     <div className="items-center px-4 justify-center sm:flex">
-                        <div className="hidden md:flex relative">
+                        <div className="hidden sm:flex relative">
                             <div className="absolute top-3 left-3 items-center">
                                 <svg className="w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
                             </div>
@@ -51,7 +48,7 @@ export default function Navbar() {
                         <ul className="hidden flex-col lg:flex lg:flex-row list-none">
                             <li className="nav-item">
                                 <a className="my-1 text-sm text-gray-900 hover:text-gray-700 font-light md:mx-4 md:my-0" href="/">
-                                    <Link href='/blog/content-1'><span className="ml-2">Blog</span></Link>
+                                    <Link href='/blog/content-1'><span className="ml-2">Documentation</span></Link>
                                 </a>
                             </li>
                             <li className="nav-item">
@@ -65,7 +62,7 @@ export default function Navbar() {
                                 </a>
                             </li>
                         </ul>
-                        <div onClick={() => setIsShown(!isShown)} className="flex justify-center px-7 py-3 md:block">
+                        <div onClick={() => setIsShown(!isShown)} className="flex justify-center px-5 py-3 md:block">
                             <a className="relative text-gray-900 hover:text-gray-700" target="_blank">
                                 <svg className="h-6 w-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M3 3H5L5.4 5M7 13H17L21 5H5.4M7 13L5.4 5M7 13L4.70711 15.2929C4.07714 15.9229 4.52331 17 5.41421 17H17M17 17C15.8954 17 15 17.8954 15 19C15 20.1046 15.8954 21 17 21C18.1046 21 19 20.1046 19 19C19 17.8954 18.1046 17 17 17ZM9 19C9 20.1046 8.10457 21 7 21C5.89543 21 5 20.1046 5 19C5 17.8954 5.89543 17 7 17C8.10457 17 9 17.8954 9 19Z" stroke="currentColor" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round"></path>
