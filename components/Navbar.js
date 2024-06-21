3adba1caf176c344de187a0df141596ca008cc2e
valoudefou <valerian.karsenty@gmail.com>
2024-06-21 22:17:41 +0100

    BURGER MENU

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,6 +8,7 @@ import Link from 'next/link'
 export default function Navbar() {
     const [isShown, setIsShown] = useContext(AppContext)
     const [cartContent, setHtmlContent] = useState(true)
+    const [burgerOn, setBurgerOn] = useState(false)
 
     useEffect(() => {
         const storedHtml = localStorage.getItem('currentProduct')
@@ -31,22 +32,24 @@ export default function Navbar() {
                 <div className="flex flex-auto items-center justify-between">
                     <nav class="sm:px-0 lg:hidden relative px-4 py-4 flex lg:justify-between items-center bg-white">
                         <div class="lg:hidden">
-                            <button class="navbar-burger flex items-center text-black p-3">
-                                <svg class="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <button onClick={() => setBurgerOn(!burgerOn)} class="navbar-burger flex items-center text-black p-3">
+                                <svg class="block h-5 w-5 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
                                 </svg>
                             </button>
                         </div>
                     </nav>
-                    <div class="navbar-menu relative z-50 hidden">
-                        <div class="navbar-backdrop fixed inset-0 bg-gray-800 opacity-25"></div>
+                    {burgerOn && (  
+                        <div class="navbar-menu relative z-50">
+                
+                        <div onClick={() => setBurgerOn(!burgerOn)} class="navbar-backdrop fixed inset-0 bg-gray-800 opacity-25"></div>  
                         <nav class="fixed top-0 left-0 bottom-0 flex flex-col w-5/6 max-w-sm py-6 px-6 bg-white border-r overflow-y-auto">
                             <div class="flex justify-between items-center mb-8">
                                 <a className="text-2xl px-4 font-bold leading-relaxed inline-block py-3 whitespace-nowrap uppercase text-gray-900" href="/">
                                     {flagIndustry.getValue()}
                                     <span className="text-sm font-thin py-1 absolute">®</span>
                                 </a>
-                                <button class="navbar-close">
+                                <button onClick={() => setBurgerOn(!burgerOn)} class="navbar-close">
                                     <svg class="h-6 w-6 text-gray-400 cursor-pointer hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
                                     </svg>
@@ -55,18 +58,19 @@ export default function Navbar() {
                             <div>
                                 <ul>
                                     <li class="mb-1">
-                                        <a class="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#">Documentation</a>
+                                        <a class="block p-4 text-sm font-normal text-gray-900 rounded" href="#">Documentation</a>
                                     </li>
                                     <li class="mb-1">
-                                        <a class="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#">Contact</a>
+                                        <a class="block p-4 text-sm font-normal text-gray-900 rounded" href="#">Contact</a>
                                     </li>
                                     <li class="mb-1">
-                                        <a class="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#">About</a>
+                                        <a class="block p-4 text-sm font-normal text-gray-900 rounded" href="#">About</a>
                                     </li>
                                 </ul>
                             </div>
-                        </nav>
+                        </nav>   
                     </div>
+                    )}
                     <div className="relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
                         <a className="text-2xl px-7 font-bold leading-relaxed inline-block py-3 whitespace-nowrap uppercase text-gray-900" href="/">
                             {flagIndustry.getValue()}
