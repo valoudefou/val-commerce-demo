c8bcfc5b2c1905de1c2682dc7d10f6b03c7d0090
valoudefou <valerian.karsenty@gmail.com>
2024-06-22 23:29:48 +0100

    TEXT

--- pages/products/index.js
+++ pages/products/index.js
@@ -8,7 +8,7 @@ export default function Index( {products} ) {
             <div className="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8 mb-24">
                 <div className="sm:py-15 mx-auto max-w-7xl py-16 px-4 sm:px-6 lg:px-8">
                     <div className="text-center">
-                        <p className="mt-1 text-3xl font-bold uppercase text-gray-900 sm:text-3xl sm:tracking-tight lg:text-3xl">
+                        <p className="mt-1 text-3xl leading-9 font-bold uppercase text-gray-900 sm:text-3xl sm:tracking-tight lg:text-3xl">
                             Shop our products
                         </p>
                     </div>
