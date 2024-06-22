4492d1cf25ae9dcc10ff68526be0846a107d601a
valoudefou <valerian.karsenty@gmail.com>
2024-06-22 02:02:18 +0100

    CONFIRMATION

--- pages/products/confirmation.js
+++ pages/products/confirmation.js
@@ -30,7 +30,7 @@ export default function Confirmation() {
         'value': data.productPrice,
         'shipping': 8.01,
         'currency': 'EUR',
-      items: [{
+        items: [{
           'item_id': data.productId,
           'item_name': data.productTitle,
           'affiliation': 'Test Drive',
@@ -62,7 +62,6 @@ export default function Confirmation() {
           </div>
           <div className="flex flex-col pl-4 pt-2 pb-1">
             <h1 className="text-2xl dark:text-white lg:text-3xl font-semibold leading-7 lg:leading-9 text-gray-800">Your order {data.transactionId} is confirmed!</h1>
-            <p className="text-base dark:text-gray-300 font-medium p-1 leading-6 text-gray-600">{data.date}</p>
           </div>
         </div>
         <div className="mt-10 flex flex-col xl:flex-row jusitfy-center items-stretch w-full xl:space-x-8 space-y-4 md:space-y-6 xl:space-y-0">
