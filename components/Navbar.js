7f744370aad5d70a6827f3d76832495d48b9d98e
valoudefou <valerian.karsenty@gmail.com>
2024-06-22 01:15:14 +0100

    INVISIBLE

--- components/Navbar.js
+++ components/Navbar.js
@@ -76,8 +76,8 @@ export default function Navbar() {
                             {flagIndustry.getValue()}
                             <span className="text-sm font-thin py-1 absolute">®</span>
                         </a>
-                    </div>
-                    <div className="items-center px-4 justify-center sm:flex">
+                    </div> 
+                    <div className={isShown ? 'invisible' : ''}>
                         <div className="hidden sm:flex relative">
                             <div className="absolute top-3 left-3 items-center">
                                 <svg className="w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
