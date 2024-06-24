f33d2981251802e77c689aa54450317970e14642
valoudefou <valerian.karsenty@gmail.com>
2024-06-24 13:21:35 +0100

    SEARCH

--- components/Navbar.js
+++ components/Navbar.js
@@ -85,33 +85,45 @@ export default function Navbar() {
                             <input type="text" suppressHydrationWarning={true} className="epoq_search_box w-96 ui-autocomplete-input block p-2 font-light pl-10 text-gray-900 bg-gray-50 rounded border border-gray-200 focus:pl-10" placeholder='Search term ...' />
                         </div>
                     </div>
-                    <div className="md:px-5 md:py-0 xl:py-0 px-3 lg:py-0 py-3 lg:flex items-center">
-                        <ul className="hidden flex-col lg:flex lg:flex-row list-none">
-                            <li className="nav-item">
-                                <a className="my-1 text-sm text-gray-900 hover:text-gray-700 font-mormal md:mx-4 md:my-0" href="#">
-                                    <Link href='/blog/content-1'><span className="ml-2">Documentation</span></Link>
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="my-1 text-sm text-gray-900 hover:text-gray-700 font-mormal md:mx-4 md:my-0" href="#">
-                                    <span className="ml-2">Contact</span>
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="my-1 text-sm text-gray-900 hover:text-gray-700 font-mormal md:mx-4 md:my-0" href="#">
-                                    <span className="ml-2">About</span>
-                                </a>
-                            </li>
-                        </ul>
-                        <div onClick={() => setIsShown(!isShown)} className="pl-3 pr-0 flex justify-center lg:pl-5 sm:px-0 cursor-pointer lg:py-3 md:block">
-                            <a className="relative text-gray-900 hover:text-gray-700" target="_blank">
-                                <svg className="h-6 w-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M3 3H5L5.4 5M7 13H17L21 5H5.4M7 13L5.4 5M7 13L4.70711 15.2929C4.07714 15.9229 4.52331 17 5.41421 17H17M17 17C15.8954 17 15 17.8954 15 19C15 20.1046 15.8954 21 17 21C18.1046 21 19 20.1046 19 19C19 17.8954 18.1046 17 17 17ZM9 19C9 20.1046 8.10457 21 7 21C5.89543 21 5 20.1046 5 19C5 17.8954 5.89543 17 7 17C8.10457 17 9 17.8954 9 19Z" stroke="currentColor" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round"></path>
+                        <div className="md:px-5 md:py-0 xl:py-0 px-3 lg:py-0 py-3 lg:flex items-center">
+                            <ul className="hidden flex-col lg:flex lg:flex-row list-none">
+                                <li className="nav-item">
+                                    <a className="my-1 text-sm text-gray-900 hover:text-gray-700 font-mormal md:mx-4 md:my-0" href="#">
+                                        <Link href='/blog/content-1'><span className="ml-2">Documentation</span></Link>
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="my-1 text-sm text-gray-900 hover:text-gray-700 font-mormal md:mx-4 md:my-0" href="#">
+                                        <span className="ml-2">Contact</span>
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="my-1 text-sm text-gray-900 hover:text-gray-700 font-mormal md:mx-4 md:my-0" href="#">
+                                        <span className="ml-2">About</span>
+                                    </a>
+                                </li>
+                            </ul>
+                            <div className="flex">
+                            <div onClick={() => setIsShown(!isShown)} className="pr-2 flex justify-center cursor-pointer">
+                                <a className="sm:hidden relative text-gray-900 hover:text-gray-700" target="_blank">
+                                <svg class="w-6 h-6 text-gray-900" stroke="white" strokeWidth="0.8" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>
                                 </svg>
-                                {cartContent && (
-                                <span style={{ backgroundColor: flagBackgroundColor.getValue() }} className="absolute top-0 left-0 rounded-full bg-indigo-100 text-gray-900 p-1 text-sm"></span>
-                                )}
-                            </a>
+                                    {cartContent && (
+                                    <span style={{ backgroundColor: flagBackgroundColor.getValue() }} className="absolute top-0 left-0 rounded-full bg-indigo-100 text-gray-900 p-1 text-sm"></span>
+                                    )}
+                                </a>
+                            </div>
+                            <div onClick={() => setIsShown(!isShown)} className="pl-3 flex justify-center lg:pl-5 sm:px-0 cursor-pointer lg:py-3 md:block">
+                                <a className="relative text-gray-900 hover:text-gray-700" target="_blank">
+                                    <svg className="h-6 w-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M3 3H5L5.4 5M7 13H17L21 5H5.4M7 13L5.4 5M7 13L4.70711 15.2929C4.07714 15.9229 4.52331 17 5.41421 17H17M17 17C15.8954 17 15 17.8954 15 19C15 20.1046 15.8954 21 17 21C18.1046 21 19 20.1046 19 19C19 17.8954 18.1046 17 17 17ZM9 19C9 20.1046 8.10457 21 7 21C5.89543 21 5 20.1046 5 19C5 17.8954 5.89543 17 7 17C8.10457 17 9 17.8954 9 19Z" stroke="currentColor" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round"></path>
+                                    </svg>
+                                    {cartContent && (
+                                    <span style={{ backgroundColor: flagBackgroundColor.getValue() }} className="absolute top-0 left-0 rounded-full bg-indigo-100 text-gray-900 p-1 text-sm"></span>
+                                    )}
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
