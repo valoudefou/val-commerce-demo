4aef80b3ecc90ec18a0cd976fa89f24d0730917a
valoudefou <valerian.karsenty@gmail.com>
2024-06-24 16:48:03 +0100

    SEARCH

--- components/Navbar.js
+++ components/Navbar.js
@@ -35,8 +35,8 @@ export default function Navbar() {
             )}
             {searchOpen && (
                 <div className="sm:hidden bg-white fixed w-full px-6 py-6 z-30 border-b-[1px] border-gray-200">
-                    <input type="text" className="epoq_search_box w-full ui-autocomplete-input block p-5 font-light pl-10 text-gray-900 bg-gray-50 rounded border border-gray-200 focus:pl-10" placeholder='Search term ...' />
-                    <div className="absolute top-12 left-9 items-center">
+                    <input type="text" className="epoq_search_box w-full ui-autocomplete-input block p-4 font-light pl-10 text-gray-900 bg-gray-50 rounded border border-gray-200 focus:pl-10" placeholder='Search term ...' />
+                    <div className="absolute top-11 left-9 items-center">
                         <svg className="w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
                     </div>
                 </div>
