959c8810104bd16cda4cecf884bd66d7779df49b
valoudefou <valerian.karsenty@gmail.com>
2024-07-08 18:09:43 +0100

    STEPS

--- pages/products/checkout.js
+++ pages/products/checkout.js
@@ -199,14 +199,9 @@ return (
                 2
               </span>
             </li>
-            <li className="flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-100 after:border-4 after:inline-block dark:after:border-gray-700">
-              <span className="text-gray-400 text-sm font-semibold flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0">
-                3
-              </span>
-            </li>
             <li className="flex items-center w-full">
               <span className="text-gray-400 text-sm font-semibold flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0">
-                4
+                3
               </span>
             </li>
           </ol>
