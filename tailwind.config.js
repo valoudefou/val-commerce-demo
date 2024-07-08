ccfd0f79f8b2645fe3a676ea09e8f63f9edb058c
valoudefou <valerian.karsenty@gmail.com>
2024-07-08 10:34:25 +0100

    MATERIAL

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,31 @@
-module.exports = {
+// module.exports = {
+//   content: [
+//     './pages/**/*.{js,ts,jsx,tsx}',
+//     './components/**/*.{js,ts,jsx,tsx}',
+//   ],
+//   plugins: [require('@tailwindcss/aspect-ratio')],
+//   theme: {
+//     extend: {
+//       colors: {
+//         'crash-yellow': '#d6ff00',
+//       },
+//     }
+//   }
+// }
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+ 
+module.exports = withMT({
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
-  plugins: [require('@tailwindcss/aspect-ratio')],
   theme: {
     extend: {
       colors: {
         'crash-yellow': '#d6ff00',
       },
-    }
-  }
-}
+    },
+  },
+  plugins: [],
+});
