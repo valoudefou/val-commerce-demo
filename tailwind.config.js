e11b88d17517ef0e81877b9b704573f99988781f
valoudefou <valerian.karsenty@gmail.com>
2024-07-08 11:53:02 +0100

    UNINSTALL

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,14 @@
-// module.exports = {
-//   content: [
-//     './pages/**/*.{js,ts,jsx,tsx}',
-//     './components/**/*.{js,ts,jsx,tsx}',
-//   ],
-//   plugins: [require('@tailwindcss/aspect-ratio')],
-//   theme: {
-//     extend: {
-//       colors: {
-//         'crash-yellow': '#d6ff00',
-//       },
-//     }
-//   }
-// }
-
-const withMT = require("@material-tailwind/react/utils/withMT");
- 
-module.exports = withMT({
+module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
+  plugins: [require('@tailwindcss/aspect-ratio')],
   theme: {
     extend: {
       colors: {
         'crash-yellow': '#d6ff00',
       },
-    },
-  },
-  plugins: [],
-});
+    }
+  }
+}
