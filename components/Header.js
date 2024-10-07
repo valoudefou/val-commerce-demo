1ff7b901cdedee7b50f964976a10c3d539024459
valoudefou <valerian.karsenty@gmail.com>
2024-10-07 18:28:48 +0100

    TEST

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,20 @@
 import Image from 'next/image'
-import { HitType, useFlagship, useFsFlag } from "@flagship.io/react-sdk"
+import { HitType, useFsFlag, useFlagship } from "@flagship.io/react-sdk"
 import Link from 'next/link'
 import Navbar from '../components/Navbar'
 
 export default function Header() {
-const fs = useFlagship()
+const fs = useFsFlag()
+const { getFlag } = useFlagship()
 
 // Get flag 
-const flagImageSrc = useFsFlag("flagImageSrc", "/jewelry.jpg")
-const flagBtnText = useFsFlag("flagBtnText", "Shop")
-const flagIndustry = useFsFlag("flagIndustry", "Product")
-const flagBackgroundColor = useFsFlag("flagBackgroundColor", "black")
-const { hit: fsHit } = useFlagship()
+const flagImageSrcVal = getFlag("flagImageSrc")
+const flagImageSrc = flagImageSrcVal.getValue("/jewelry.jpg")
+const flagBtnTextVal = getFlag("flagBtnText", "Shop")
+const flagBtnText = flagBtnTextVal.getValue()
+const flagIndustryVal = getFlag("flagIndustry", "Product")
+const flagIndustry = flagIndustryVal.getValue()
+const flagBackgroundColor = getFlag("flagBackgroundColor", "black")
 
   return (
     <header className="relative">
@@ -24,23 +27,23 @@ const { hit: fsHit } = useFlagship()
                 priority
                 layout='fill'
                 className="h-full w-full object-cover"
-                src={flagImageSrc.getValue()}
-                alt={flagIndustry.getValue()}
+                src={flagImageSrc}
+                alt={flagIndustry}
               />
               <div className="absolute inset-0" />
             </div>
             <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
             <p className="mx-auto max-w-xl text-center text-xl font-semibold uppercase tracking-wide text-white">
-            {'The ' + flagIndustry.getValue() + ' House'}
+            {'The ' + flagIndustry + ' House'}
             </p>
             <h1 className="mt-1 text-center font-semibold text-gray-900 text-4xl sm:text-5xl">
             <span className="block text-white">Life is better with</span>
-            <span className="block" style={{color: flagBackgroundColor.getValue()}}>{flagIndustry.getValue()}</span>
+            <span className="block" style={{color: flagBackgroundColor.getValue()}}>{flagIndustry}</span>
             </h1>
             <div className="mt-10 flex justify-center">
               <Link href="/products">
                 <button onClick={()=>{
-                    fsHit.send({
+                    fs.sendHits({
                       type: HitType.EVENT,
                       category: "Action Tracking",
                       action: "Click Discover",
@@ -51,7 +54,7 @@ const { hit: fsHit } = useFlagship()
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1" stroke="currentColor" className="w-6 h-6 py-1">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"/>
                   </svg>
-                  {flagBtnText.getValue()}
+                  {flagBtnText}
                 </button>
               </Link>
             </div>
