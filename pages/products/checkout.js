41cb4828c60c747288ce10a64793a3a17664db82
valoudefou <valerian.karsenty@gmail.com>
2024-10-07 17:11:12 +0100

    CHECKOUT

--- pages/products/checkout.js
+++ pages/products/checkout.js
@@ -6,7 +6,6 @@ export default function Checkout() {
   // Get flag 
   const paymentFeature1Click = useFsFlag("paymentFeature1Click", "false")
   const flagIndustry = useFsFlag("flagIndustry", "Product")
-  const flagColorLine = useFsFlag("flagColorLine", "after:border-black")
   const flagDeliveryFeeDpd = useFsFlag("flagDeliveryFeeDpd", 7.99)
   const flagDeliveryFeeEvri = useFsFlag("flagDeliveryFeeEvri", 3.99)
   // KEY
@@ -263,59 +262,55 @@ export default function Checkout() {
           </div>
           {!paymentStep && (
             <div className="mt-8 px-3">
-              <ol className="flex items-center w-full">
-                <li className={"flex w-full items-center text-blue-600 dark:text-blue-500 after:content-[''] after:w-full after:h-1 after:border-b " + flagColorLine.getValue() + " after:border-4 after:inline-block dark:after:border-blue-800"}>
-                  <span className="flex items-center justify-center w-10 h-10 bg-black rounded-full lg:h-12 lg:w-12 dark:bg-blue-800 shrink-0">
-                    <svg className="w-3.5 h-3.5 text-white lg:w-4 lg:h-4 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
-                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5.917 5.724 10.5 15 1.5"/>
-                    </svg>
-                  </span>
-                </li>
-                <li className="flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-100 after:border-4 after:inline-block dark:after:border-gray-700">
-                  <span className="text-white text-sm font-semibold flex items-center justify-center w-10 h-10 bg-black rounded-full lg:h-12 lg:w-12 dark:bg-blue-800 shrink-0">
-                    2
-                  </span>
-                </li>
-                <li className="flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-100 after:border-4 after:inline-block dark:after:border-gray-700">
-                  <span className="text-gray-400 text-sm font-semibold flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0">
-                    3
-                  </span>
-                </li>
-                <li className="flex items-center w-full">
-                  <span className="text-gray-400 text-sm font-semibold flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0">
-                    4
-                  </span>
-                </li>
+              <ol class="flex items-center w-full">
+                  <li class="flex w-full items-center text-blue-600 dark:text-blue-500 after:content-[''] after:w-full after:h-1 after:border-b after:border-black after:border-4 after:inline-block dark:after:border-blue-800">
+                      <span class="flex items-center justify-center w-10 h-10 bg-black rounded-full lg:h-12 lg:w-12 dark:bg-blue-800 shrink-0">
+                          <svg class="w-3.5 h-3.5 text-white lg:w-4 lg:h-4 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5"/>
+                          </svg>
+                      </span>
+                  </li>
+                  <li class="flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-100 after:border-4 after:inline-block dark:after:border-gray-700">
+                      <span class="flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0">
+                          <svg class="w-4 h-4 text-gray-500 lg:w-5 lg:h-5 dark:text-gray-100" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
+                              <path d="M18 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2ZM6.5 3a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5ZM3.014 13.021l.157-.625A3.427 3.427 0 0 1 6.5 9.571a3.426 3.426 0 0 1 3.322 2.805l.159.622-6.967.023ZM16 12h-3a1 1 0 0 1 0-2h3a1 1 0 0 1 0 2Zm0-3h-3a1 1 0 1 1 0-2h3a1 1 0 1 1 0 2Zm0-3h-3a1 1 0 1 1 0-2h3a1 1 0 1 1 0 2Z"/>
+                          </svg>
+                      </span>
+                  </li>
+                  <li class="flex items-center w-full">
+                      <span class="flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0">
+                          <svg class="w-4 h-4 text-gray-500 lg:w-5 lg:h-5 dark:text-gray-100" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+                              <path d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2ZM7 2h4v3H7V2Zm5.7 8.289-3.975 3.857a1 1 0 0 1-1.393 0L5.3 12.182a1.002 1.002 0 1 1 1.4-1.436l1.328 1.289 3.28-3.181a1 1 0 1 1 1.392 1.435Z"/>
+                          </svg>
+                      </span>
+                  </li>
               </ol>
             </div>
           )}
           {paymentStep && (
             <div className="mt-8 px-3">
-              <ol className="flex items-center w-full">
-                <li className={"flex w-full items-center text-blue-600 dark:text-blue-500 after:content-[''] after:w-full after:h-1 after:border-b " + flagColorLine.getValue() + " after:border-4 after:inline-block dark:after:border-blue-800"}>
-                  <span className="flex items-center justify-center w-10 h-10 bg-black rounded-full lg:h-12 lg:w-12 dark:bg-blue-800 shrink-0">
-                    <svg className="w-3.5 h-3.5 text-white lg:w-4 lg:h-4 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
-                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5.917 5.724 10.5 15 1.5"/>
-                    </svg>
-                  </span>
-                </li>
-                <li className={"flex w-full items-center text-blue-600 dark:text-blue-500 after:content-[''] after:w-full after:h-1 after:border-b " + flagColorLine.getValue() + " after:border-4 after:inline-block dark:after:border-blue-800"}>
-                  <span className="text-white text-sm font-semibold flex items-center justify-center w-10 h-10 bg-black rounded-full lg:h-12 lg:w-12 dark:bg-blue-800 shrink-0">
-                    <svg className="w-3.5 h-3.5 text-white lg:w-4 lg:h-4 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
-                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5.917 5.724 10.5 15 1.5"/>
-                    </svg>
-                  </span>
-                </li>
-                <li className="flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-100 after:border-4 after:inline-block dark:after:border-gray-700">
-                  <span className="text-white text-sm font-semibold flex items-center justify-center w-10 h-10 bg-black rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0">
-                    3
-                  </span>
-                </li>
-                <li className="flex items-center w-full">
-                  <span className="text-gray-400 text-sm font-semibold flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0">
-                    4
-                  </span>
-                </li>
+              <ol class="flex items-center w-full">
+                  <li class="flex w-full items-center text-blue-600 dark:text-blue-500 after:content-[''] after:w-full after:h-1 after:border-b after:border-black after:border-4 after:inline-block dark:after:border-blue-800">
+                      <span class="flex items-center justify-center w-10 h-10 bg-black rounded-full lg:h-12 lg:w-12 dark:bg-blue-800 shrink-0">
+                          <svg class="w-3.5 h-3.5 text-white lg:w-4 lg:h-4 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5"/>
+                          </svg>
+                      </span>
+                  </li>
+                  <li class="flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-black after:border-4 after:inline-block dark:after:border-gray-700">
+                      <span class="flex items-center justify-center w-10 h-10 bg-black rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0">
+                          <svg class="w-4 h-4 text-white lg:w-5 lg:h-5 dark:text-gray-100" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
+                              <path d="M18 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2ZM6.5 3a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5ZM3.014 13.021l.157-.625A3.427 3.427 0 0 1 6.5 9.571a3.426 3.426 0 0 1 3.322 2.805l.159.622-6.967.023ZM16 12h-3a1 1 0 0 1 0-2h3a1 1 0 0 1 0 2Zm0-3h-3a1 1 0 1 1 0-2h3a1 1 0 1 1 0 2Zm0-3h-3a1 1 0 1 1 0-2h3a1 1 0 1 1 0 2Z"/>
+                          </svg>
+                      </span>
+                  </li>
+                  <li class="flex items-center w-full">
+                      <span class="flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0">
+                          <svg class="w-4 h-4 text-gray-500 lg:w-5 lg:h-5 dark:text-gray-100" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+                              <path d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2ZM7 2h4v3H7V2Zm5.7 8.289-3.975 3.857a1 1 0 0 1-1.393 0L5.3 12.182a1.002 1.002 0 1 1 1.4-1.436l1.328 1.289 3.28-3.181a1 1 0 1 1 1.392 1.435Z"/>
+                          </svg>
+                      </span>
+                  </li>
               </ol>
             </div>
           )}
@@ -837,19 +832,9 @@ export default function Checkout() {
           <div class="w-72">
             <div class="grid grid-cols-1 gap-4 py-10 px-4">
               <div class="pt-2 text-3xl font-semibold text-gray-900">Cart</div>
-                <p class="grid grid-cols-1 gap-3">
-                  The cart is empty
-                </p>
-              <div>
-                <Link href="/products">
-                  <button class="flex items-center justify-center bg-white text-slate-600 text-bold text-sm font-medium">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="w-6 h-6 py-1">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"></path>
-                    </svg>
-                    Browse products
-                  </button>
-                </Link>
-              </div>
+              <p class="grid grid-cols-1 gap-3">
+                The cart is empty
+              </p>
             </div>
           </div>
         }
