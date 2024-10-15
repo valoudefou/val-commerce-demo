a6c7878731c577711372324a44d027ece525f39d
valoudefou <valerian.karsenty@gmail.com>
2024-10-15 08:53:55 +0100

    SEARCH

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -29,10 +29,10 @@ export default function SearchResults() {
         <>
             <Navbar />
             <div className="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8 mb-24">
-                <div className="sm:py-15 mx-auto max-w-7xl py-16 px-4 sm:px-6 lg:px-8">
+                <div className="sm:py-12 mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:px-8">
                     <div className="text-center">
-                        <div className="mt-1 flex justify-center font-medium text-2xl leading-9 text-gray-900 sm:text-2xl sm:tracking-tight lg:text-2xl">
-                            <span>{totalCount.current > 0 ? 'search results ' + '"' + searchQuery + '"' : 'No results'}</span>
+                        <div className="flex justify-center font-medium text-2xl leading-9 text-gray-900 sm:text-2xl sm:tracking-tight lg:text-2xl">
+                            <span>{totalCount.current > 0 ? ' ' : 'No results'}</span>
                         </div>
                     </div>
                 </div>
