58f6b8ae5ff2841d6fcf344505701b72648a6abb
valoudefou <valerian.karsenty@gmail.com>
2024-11-07 21:55:12 +0000

    UPDATE

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import { useFsFlag } from "@flagship.io/react-sdk"
 import Link from 'next/link'
 
 export default function Footer({ children }) {
-    const flagBackgroundColorVal = useFsFlag("flagBackgroundColor")
+    const flagBackgroundColorVal = useFsFlag("//flagBackgroundColor")
     const flagBackgroundColor = flagBackgroundColorVal.getValue("white")
 
     return (
@@ -12,7 +12,7 @@ export default function Footer({ children }) {
             </main>
             <footer className="mt-auto items-center flex justify-between space-x-3 p-4 text-xs">
                     <span>
-                        &copy; AB Tasty / Next.js / Tailwind
+                        &copy; AB Tasty / NextJS / Tailwind
                     </span>
                 <Link href="https://github.com/valoudefou/commerce-demo" style={{color: flagBackgroundColor}} className="py-1 px-2 font-medium text-xs bg-[#111827] rounded-[5px]">
                     GitHub
