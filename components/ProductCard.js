7de6b3b0456d4702e7ca1f2aff438c9d7a858ef8
valoudefou <valerian.karsenty@gmail.com>
2024-11-16 09:44:19 +0000

    TEST

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -14,6 +14,8 @@ function ProductCard( {product} ) {
         return classes.filter(Boolean).join(' ')
     }
 
+    alert('test')
+
     return (
         <Link href={`/products/${product.id}`} className="group">
             <div className='cursor-pointer flex flex-col'>
