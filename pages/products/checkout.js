782614d0dd000182c08299658be1fa67447bc5f8
valoudefou <valerian.karsenty@gmail.com>
2024-11-18 17:24:12 +0000

    UPDATE

--- pages/products/checkout.js
+++ pages/products/checkout.js
@@ -819,7 +819,7 @@ export default function Checkout() {
                   <div className="flex border-gray-200">
                     <input className="border border-slate-300 rounded-l-2xl w-full py-4 px-4 text-grey-darker" id="coupon" type="coupon" placeholder="Coupon code"/>
                     <button onClick={handleApply} className="bg-black hover:bg-blue-dark text-white rounded-r-2xl text-sm font-medium px-7" type="submit">
-                      Apply
+                      Apply.
                     </button>
                   </div>
                   <h3 className="text-xl dark:text-white font-semibold leading-5 text-gray-800">
