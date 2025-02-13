52eb24835f2b9e7e18767f035ced2e082f62b97a
valoudefou <valerian.karsenty@gmail.com>
2025-02-13 23:57:20 +0000

    UPDATE

--- next.config.js
+++ next.config.js
@@ -2,4 +2,16 @@ module.exports = {
   images: {
     domains: ['cdn.dummyjson.com', 'api.getaddress.io'],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/(.*)",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "https://live-server1.com" },
+          { key: "Access-Control-Allow-Methods", value: "GET,POST,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" }
+        ]
+      }
+    ];
+  }
 }
