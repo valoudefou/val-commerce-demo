49b2e1a72f210b859819765f2b00e03e74bb7d49
valoudefou <valerian.karsenty@gmail.com>
2025-08-04 22:45:10 +0100

    Update

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -209,7 +209,7 @@ export default function Product({ product }) {
                       <svg className="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4m0 0L7 13m0 0l-1.5 6M7 13l-1.5-6m0 0h15M17 13v6a2 2 0 01-2 2H9a2 2 0 01-2-2v-6" />
                       </svg>
-                      <span>Add to Cart - Enhanced</span>
+                      <span>Add to Cart</span>
                     </div>
                   </button>
 
